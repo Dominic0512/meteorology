185f5e35233ee33d206261cbc1a6fefbfe4817be
dominic <dominic@thetigerparty.com>
2015-06-07 23:39:50 +0800

    Fixed http request issue

--- app/src/main/java/com/meteorology/meteorology/MainActivity.java
+++ app/src/main/java/com/meteorology/meteorology/MainActivity.java
@@ -6,17 +6,27 @@ import com.meteorology.meteorology.Model.City;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends Activity {
-
+    String test = "ttt  ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,22 +59,58 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
     public void updateDatabase() {
-        City city = new City();
-        city.name = "Fuck u";
-        city.save();
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    String url = "http://www.google.com/";
+                    String USER_AGENT = "Mozilla/5.0";
+
+                    URL obj = new URL(url);
+                    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                    // optional default is GET
+                    con.setRequestMethod("GET");
+
+                    //add request header
+                    con.setRequestProperty("User-Agent", USER_AGENT);
+
+                    int responseCode = con.getResponseCode();
+                    System.out.println("\nSending 'GET' request to URL : " + url);
+                    System.out.println("Response Code : " + responseCode);
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+                    test = response.toString();
+                    //print result
+                    Log.d("HTML", test);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 
     public void initMainView() {
         LinearLayout container = (LinearLayout) findViewById(R.id.container);
         TextView console = new TextView(this);
         String all = "";
-        List<City> cities = new ArrayList<>();
-        cities = City.getAll();
-        for( int i = 0; i < cities.size(); i++ ) {
-            all += cities.get(i).name;
-        }
+//        List<City> cities = new ArrayList<>();
+//        cities = City.getAll();
+//        for( int i = 0; i < cities.size(); i++ ) {
+//            all += cities.get(i).name;
+//        }
 
-        console.setText(all);
+        console.setText(test);
         container.addView(console);
     }
 }
